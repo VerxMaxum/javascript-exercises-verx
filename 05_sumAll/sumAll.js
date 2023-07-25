5b5de05204e49b81d7b2b7e7afa22ece04b18a98
VerxMaxum <138268061+VerxMaxum@users.noreply.github.com>
2023-07-25 14:31:21 +0800

    Solve sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    const typeStart = typeof start;
+    const typeEnd = typeof end;
+    if(typeStart != "number") return "ERROR";
+    if(typeEnd != "number") return "ERROR";
+    if(start < 0 || end < 0) return "ERROR";
 
+    let sum = 0;
+    if(start < end) {
+        for(let i = start; i <= end; i++) {
+            sum += i;
+        }
+    }
+    else if(start > end) {
+        for(let i = start; i >= end; i--) {
+            sum += i;
+        }
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
