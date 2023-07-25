b36d28111af4ef85ae368cdca29e2f3ba1ba1d21
VerxMaxum <138268061+VerxMaxum@users.noreply.github.com>
2023-07-25 15:08:54 +0800

    Solve tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celcius = (fahrenheit - 32) * (5/9);
+  if(celcius % 1 === 0) return celcius;
+  return parseFloat(celcius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  let fahrenheit = (celcius * 1.8) + 32;
+  if(fahrenheit % 1 === 0) return fahrenheit;
+  return parseFloat(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
