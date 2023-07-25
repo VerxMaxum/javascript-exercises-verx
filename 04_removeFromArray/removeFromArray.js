f3419115e8a6c8aec3955ae36968c518a1ab08fb
VerxMaxum <138268061+VerxMaxum@users.noreply.github.com>
2023-07-25 14:31:51 +0800

    Solve removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...toRemove) {
+    const newArr = [];
+    const remove = Array.from(toRemove);
 
+    outer:
+    for(let i = 0; i < arr.length; i++) {
+        for(let j = 0; j < remove.length; j++) {
+            if(arr[i] === toRemove[j]) continue outer;
+        }
+        newArr.push(arr[i]);
+    }
+
+    return newArr;
 };
 
 // Do not edit below this line
